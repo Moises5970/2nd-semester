5d0764c4a82ae2f43d3b96caf0ce926af930c8e9
Moisés Martínez <153334230+Moises5970@users.noreply.github.com>
2025-04-10 09:30:01 -0600

    up

--- P.O.O/3er parcial/clas_abs_interfaces.js
+++ P.O.O/3er parcial/clas_abs_interfaces.js
@@ -37,3 +37,53 @@ const bocina1 = new JBL("JBL","rojo","Go 4");
 bocina1.encender();
 bocina1.hacerSonido();
 bocina1.apagar();
+
+//ejemplo 2
+// Clase abstracta
+class Vehiculo {
+    constructor() {
+        if (this.constructor === Vehiculo) {
+            throw new Error("No se puede instanciar una clase abstracta.");
+        }
+    }
+
+    arrancar() {
+        throw new Error("Método abstracto 'arrancar' debe ser implementado.");
+    }
+
+    conducir() {
+        throw new Error("Método abstracto 'conducir' debe ser implementado.");
+    }
+
+    detenerse() {
+        throw new Error("Método abstracto 'detenerse' debe ser implementado.");
+    }
+}
+
+// Clase concreta que implementa Vehiculo
+class Auto extends Vehiculo {
+    constructor(marca, modelo, año) {
+        super();
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
+    arrancar() {
+        console.log(`El auto ${this.marca} ${this.modelo} está arrancando.`);
+    }
+
+    conducir() {
+        console.log(`Conduciendo el ${this.marca} del año ${this.año}.`);
+    }
+
+    detenerse() {
+        console.log(`El auto ${this.modelo} se ha detenido.`);
+    }
+}
+
+// Crear una instancia de Auto
+const miAuto = new Auto("Toyota", "Corolla", 2022);
+miAuto.arrancar();
+miAuto.conducir();
+miAuto.detenerse();
